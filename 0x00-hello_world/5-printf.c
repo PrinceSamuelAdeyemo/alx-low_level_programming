84c64f9c1536e0ab7dcd852d4cc4db1e3782c5f5
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2022-03-17 17:37:07 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*This is to output in C using printf*/
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*Size of varipus data types in C programming language*/
-int main(void) {
+int main(void) 
+{
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long: %d byte(s)\n", sizeof(long int));
