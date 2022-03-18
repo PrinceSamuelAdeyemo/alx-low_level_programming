483a4cbe90b9922f63b3bcc9076b1f04faeea21a
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2022-03-17 17:45:21 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*This is to output in C using printf*/
+/*This is to output in C using printf
+ * main is the beginning of the function
+ * printf is the output statement
+ * return ends the program*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
