e1d2214c71dd086fbe811f0afb0ad520fa7fa7c1
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2022-03-23 14:24:09 -0700

    Task zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * variable used - while loop
+ * Return: Allow 0 (Success)
+ */
+if (n == 0)
+{
+	printf("%d is zero\n", n);
+}
+else if (n > 0)
+{
+	printf("%d is positive\n", n);
+}
+else
+{
+	printf("%d is negative\n", n);
+}
